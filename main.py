8408bc287f1e4c598f5b9c52830a040f4c9f4611
chistyinfo <chistyinfo@gmail.com>
2023-01-24 07:11:36 +0600

    first

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
-from backtesting import Backtest, Strategy
-from backtesting.lib import crossover
-
-from backtesting.test import SMA
-
+from backtesting import Backtest
 import pandas as pd
 
-# Load the stock data into a DataFrame
-from stop_loss import Start
+from RsiOscillator import RsiOscillator
+from SmaCross import SmaCross
 
-df = pd.read_csv("stock_data_g.csv", index_col='Time', parse_dates=True)
-
+df = pd.read_csv("stock_data_bpml.csv", index_col='Time', parse_dates=True)
 
 # print(df)
 
-
-class SmaCross(Strategy):
-    def init(self):
-        price = self.data.Close
-        self.ma1 = self.I(SMA, price, 9)
-        self.ma2 = self.I(SMA, price, 20)
-
-    def next(self):
-        if crossover(self.ma1, self.ma2):
-            self.buy()
-        elif crossover(self.ma2, self.ma1):
-            self.sell()
-
-
-Start
-# Backtest for stop loss
-bt = Backtest(df, Start, cash=100000, commission=.0035,
-              exclusive_orders=True)
-
-# Backtest sma cross over
-ct = Backtest(df, SmaCross, cash=100000, commission=.0035,
+bt = Backtest(df, SmaCross, cash=100000, commission=.0035,
               exclusive_orders=True)
 
 stats = bt.run()
-tats = ct.run()
-print(stats, tats)
-bt.plot()
-ct.plot()
+print(stats)
+#bt.plot()
+
+
